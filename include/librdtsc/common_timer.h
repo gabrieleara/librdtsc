2d5cac4ca349862c5872e794aa52d3e6045fc766
Gabriele Ara <gabriele.ara@santannapisa.it>
2021-04-01 10:13:14 +0200

    Easier to track elapsed time

--- include/librdtsc/common_timer.h
+++ include/librdtsc/common_timer.h
@@ -11,4 +11,37 @@ extern int rdtsc_init();
 extern void rdtsc_set_tsc_freq();
 extern uint64_t rdtsc_get_tsc_freq();
 
+#define MS_PER_S 1000
+#define US_PER_S 1000000
+#define NS_PER_S 1000000000
+
+static inline uint64_t rdtsc_elapsed_unit(uint64_t before, uint64_t after,
+                                          uint64_t multiplier) {
+#define static_cast(t, x) ((t)(x))
+    register uint64_t hz = rdtsc_get_tsc_hz();
+    if (after < before)
+        return 0;
+
+    return (static_cast(double, after - before) *
+            static_cast(double, multiplier)) /
+           static_cast(double, hz);
+#undef static_cast
+}
+
+static inline uint64_t rdtsc_elapsed_ns(uint64_t before, uint64_t after) {
+    return rdtsc_elapsed_unit(before, after, NS_PER_S);
+}
+
+static inline uint64_t rdtsc_elapsed_us(uint64_t before, uint64_t after) {
+    return rdtsc_elapsed_unit(before, after, US_PER_S);
+}
+
+static inline uint64_t rdtsc_elapsed_ms(uint64_t before, uint64_t after) {
+    return rdtsc_elapsed_unit(before, after, MS_PER_S);
+}
+
+static inline uint64_t rdtsc_elapsed_s(uint64_t before, uint64_t after) {
+    return rdtsc_elapsed_unit(before, after, 1);
+}
+
 #endif
